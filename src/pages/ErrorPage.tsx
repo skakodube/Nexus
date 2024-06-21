be1ca2ff0c3d86d967a2d1e9f4293d9374e49bd8
Skakodube <skakodube@gmail.com>
2024-06-20 21:46:51 -0500

    Handle errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      backgroundImage="url('https://images.hdqwalls.com/wallpapers/pubg-abandoned-city-3r.jpg')" // Replace with your image URL
+      backgroundSize="cover"
+      backgroundPosition="center"
+      position="relative"
+      filter="grayscale(90%)"
+    >
+      <Grid
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        bgGradient="linear(to-r, gray.800, rgba(21, 21, 21, 0.9), rgba(21, 21, 21, 0.5))"
+        templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)" }}
+        paddingX={{ base: "none", lg: 20 }}
+      >
+        <Center height={"100vh"}>
+          <VStack alignItems={"left"} mb={20}>
+            <Heading fontSize={{ base: 150, lg: 190 }} fontWeight={"500"}>
+              404
+            </Heading>
+            <Text whiteSpace="pre-line" fontSize={20} fontWeight={"600"}>
+              {isRouteErrorResponse(error)
+                ? `Whoops! \nWe couldn't find that page.`
+                : "An unexpected error occurred."}
+            </Text>
+            <Button
+              mt={20}
+              fontWeight={"normal"}
+              fontSize={"lg"}
+              colorScheme="gray"
+              paddingY={6}
+            >
+              Main Page
+            </Button>
+          </VStack>
+        </Center>
+      </Grid>
+    </Box>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import GameCollection from "./pages/GameCollection";
 import GamePage from "./pages/GamePage";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
